a3aeff25231e735c29cfdef822bacc6157939aea
JessC <jesse.callwood@gmail.com>
2015-10-23 15:39:02 -0700

    change migration timestamps

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 
 def index
 	@posts = Post.all
-	@user = "USER NAME TO GO HERE EVENTUALLY...maybe"
+	# @user = "USER NAME TO GO HERE EVENTUALLY...maybe"
 end
 
 def new  
