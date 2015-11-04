ac5ad3dedc95637116b5cd692582b8da0701256e
JessC <jesse.callwood@gmail.com>
2015-11-03 19:24:55 -0800

    commit 21 for s3 db test

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,10 @@ class Post < ActiveRecord::Base
 	validates :image, presence: true
 
   has_attached_file :storage => :s3,
-  									:s3_host_name => 's3-us-west-1.amazonaws.com',
-          					:s3_credentials => Proc.new{|a| a.instance.s3_credentials }
-  									# :image, styles: { :medium => "640x" }
-
+          					:s3_credentials => Proc.new{|a| a.instance.s3_credentials },
+										:s3_host_name => 's3-us-west-1.amazonaws.com',
+  									:image, styles: { :medium => "640x" }
+  									
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   #**********************************************************************************************
      #SAYS S3 CREDENTIALS NEED TO BE IN MODEL, BUT VERIFY LATER, IF NOT DELETE!!!
