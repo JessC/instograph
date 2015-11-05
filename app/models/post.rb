4e576aeb535a8532bd5026737db4301443cd80e2
JessC <jesse.callwood@gmail.com>
2015-11-04 19:28:50 -0800

    commit 33 for s3 db test

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,14 @@
 class Post < ActiveRecord::Base
 	validates :image, presence: true
 
-  has_attached_file :image, styles: { :medium => "640x" }
+  has_attached_file :image, 
+  									:storage => s3,
+  									:s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+  									styles: { :medium => "640x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+  def s3_credentials
+    {:bucket => "elasticbeanstalk-us-west-1-467246738516", :access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']}
+  end
+
 end
