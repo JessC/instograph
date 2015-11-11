74d0695d85b78b8e130e6ddda2225a5809a620e0
JessC <jesse.callwood@gmail.com>
2015-11-10 18:00:42 -0800

    commit 39.1 for s3 db test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   # before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :set_s3_direct_post, only: [:new, :edit, :create, :update]
+  # before_action :set_s3_direct_post, only: [:new, :edit, :create, :update]
 
 def index
 	@posts = Post.all
@@ -42,8 +42,8 @@ def post_params
 	params.require(:post).permit(:image, :caption)
 end
 
-def set_s3_direct_post
-  @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
-end
+# def set_s3_direct_post
+#   @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
+# end
 
 end
