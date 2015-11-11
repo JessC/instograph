8eb67d047c062530220089bd1965f9f1041af5d0
JessC <jesse.callwood@gmail.com>
2015-11-10 17:59:50 -0800

    commit 39 for s3 db test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,8 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
+  s3 = AWS::S3.new
+
   Paperclip.options[:command_path] = "/usr/local/bin/"
 
   # Amazon Web Services S3
