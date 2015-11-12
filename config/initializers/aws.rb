89877b940eeac3353498c6a8aca267079bed78da
JessC <jesse.callwood@gmail.com>
2015-11-11 17:27:55 -0800

    commit 41 for s3 db test

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,7 +1,7 @@
 require 'aws-sdk'
 
 AWS.config(:s3_endpoint => 'elasticbeanstalk-us-west-1-467246738516.s3.amazonaws.com')
-s3 = AWS::S3.new
+# s3 = AWS::S3.new
   
 #ADDED 11/20/2015
 #DO I NEED THIS FILE, FIND OUT AND DELETE IF NOT
