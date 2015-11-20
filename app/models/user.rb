466968185d17b22d4f80a326ff50bdca6dc1c4d1
JessC <jesse.callwood@gmail.com>
2015-11-20 14:10:45 -0800

    change table naming to troubleshoot deploy error2

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   has_many :friends, through: :friendships
 
   has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id"
-  has_many :inverse_friends, through: :inverse_friendships, source: :user
+  has_many :inverse_friends, through: :inverse_friendships, source: :users
 
   validates_uniqueness_of :email
   validates_uniqueness_of :username
