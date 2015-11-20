d3746fb5a4cabb5cb495320b7d7f1390a70cb9b9
JessC <jesse.callwood@gmail.com>
2015-11-20 14:14:03 -0800

    change table naming to troubleshoot deploy error3

--- db/migrate/20151007012108_create_friendships.rb
+++ db/migrate/20151007012108_create_friendships.rb
@@ -1,7 +1,7 @@
 class CreateFriendships < ActiveRecord::Migration
   def change
     create_table :friendships do |t|
-      t.references :users, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
       t.references :friends, index: true, foreign_key: true
 
       t.timestamps null: false
