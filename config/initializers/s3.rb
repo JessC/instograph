97a95554618a5a363786d875f917cec9c5a45222
JessC <jesse.callwood@gmail.com>
2015-12-06 04:55:44 -0800

    change yml env from previous commit

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -2,11 +2,11 @@ CarrierWave.configure do |config|
 	# config.fog_provider = 'fog/aws' 
 	config.fog_credentials = {
 		provider:               'AWS',
-		aws_access_key_id:      'AKIAI4YT4VOBBYXA2IBA',
-		aws_secret_access_key:  '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',
+		aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
+		aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'],
 		region:                 'us-west-1' #default is us-east-1...booo
 	}
-	config.fog_directory =  'instograph-content'
+	config.fog_directory = 'instograph-content'
   # config.cache_dir =      "#{Rails.root}/tmp/uploads"
 
 end
